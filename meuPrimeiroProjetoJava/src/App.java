b83405bb653d5e329163a89fbc2b34de912045b2
eribertobruno <eribertobruno@Gmail.com>
2024-01-17 15:10:52 -0300

    Criando projeto para exemplos de tipos e variaveis

--- meuPrimeiroProjetoJava/src/App.java
+++ meuPrimeiroProjetoJava/src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Boas vindas turma DIO!");
+        System.out.println(nomeCompleto("Eriberto ", "Bruno"));
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(segundoNome);
     }
 }
